33d46d8d9322f8bad3db24e03925144bc151c587
WestBalance <mr.bluerider3@gmail.com>
2025-09-28 23:06:14 +0300

    Fixed bugs

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
+import { CarProps } from "@/types";
 
 export  async function fetchCars() {
     const headers ={
@@ -34,3 +35,7 @@ export const calculateCarRent = (city_mpg:number, year:number) => {
 
     return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = (car: CarProps, angle? : string) =>{
+    const url = new URL('https://cdn.imagin.studio/getimage')
+}
